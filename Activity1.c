7648c81ae3b205cda9db988909d0ff66573f7060
Arthur Brito Meira Ianatoni <tucabrito08@gmail.com>
2025-06-13 13:00:12 -0400

    Create Activity1.c
    This is my first code! this activity was my introduction to C, I did it with my friend.

---
+++ Activity1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+   int main(){
+int n1, n2 ;
+
+     
+     printf(" What is the first value? \n");
+  scanf("%d", &n1);
+
+printf("What is the second value? \n");
+  scanf("%d", &n2);
+
+int sum = n1+n2;
+
+ printf(" Thes sum of the values is equal to: %d", sum);
+
+   }
